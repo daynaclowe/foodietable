3f104dd9bc384aaa54f08f3da4cfb820a077eadd
MitsuyoshiToguri <seiun340066@gmail.com>
2014-11-13 15:10:25 -0500

    I has added detail(show edit destroy create) at  controller and added belongs_to :restaurant :user

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,28 @@
 class ReviewsController < ApplicationController
-  def show
-  end
 
-  def create
-  end
 
-  def destroy
-  end
+	def show
+		@review = Review.find(params[:id])
+	end
 
-  def edit
-  end
-end
+	def edit 
+		@review = Review.find(params[:id])
+
+		def create
+			@review = @foodie.reviews.build(review_params)
+			@review.user = current_user
+
+
+			if @review.save
+				redirect_to foody_path, notice: 'Review created successfully'
+			else
+				render 'foodies/show'
+			end
+		end
+
+		def destroy
+			@review = Review.find(params[:id])
+			@review.destroy
+		end
+
+	end
