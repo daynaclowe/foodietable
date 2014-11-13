b39da1a3791bb934093f7796b17b7c01d11e2949
chaugirl <chau.marialuisa@gmail.com>
2014-11-13 17:46:39 -0500

    Added views and controller.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141113195039) do
+ActiveRecord::Schema.define(version: 20141113221026) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 20141113195039) do
     t.datetime "res_time"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
+    t.string   "email_address"
   end
 
 end
