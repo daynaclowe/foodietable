242be174a947c0190fcacef8d08fbbf32cd4e761
MitsuyoshiToguri <seiun340066@gmail.com>
2014-11-13 14:52:16 -0500

    Generating the reviewt model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141113184946) do
+ActiveRecord::Schema.define(version: 20141113194941) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20141113184946) do
     t.string   "password"
     t.string   "email"
     t.text     "bio"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "reviews", force: true do |t|
+    t.string   "title"
+    t.integer  "rating"
+    t.text     "comments"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
